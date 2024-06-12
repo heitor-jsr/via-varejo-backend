a53c0beec9d456d1d87c80f8a7314b0de1f957ce
Heitor José <heitorscalon@gmail.com>
2024-06-11 21:33:52 -0300

    feat: added GetRedisPurchaseSummary method to handler

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -6,6 +6,7 @@ import (
 	"via-varejo/helpers"
 	"via-varejo/internal/domain"
 
+	"github.com/go-chi/chi"
 	"github.com/gofrs/uuid"
 )
 
@@ -17,23 +18,24 @@ func CreateNewPurchaseSummary(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	newPurchaseSummaryResponse, err := processPurchaseSummary(newPurchaseSummary)
+	newPurchaseSummaryResponse, purchaseID, err := processPurchaseSummary(newPurchaseSummary)
 	if err != nil {
 		helpers.ErrorJSON(w, err, http.StatusInternalServerError)
 		return
 	}
 
 	helpers.WriteJSON(w, http.StatusOK, newPurchaseSummaryResponse)
+	http.Redirect(w, r, "/purchases/"+purchaseID.String(), http.StatusSeeOther)
 }
 
-func processPurchaseSummary(newPurchaseSummary domain.PurchaseSummary) (domain.PurchaseSummaryResponse, error) {
+func processPurchaseSummary(newPurchaseSummary domain.PurchaseSummary) (domain.PurchaseSummaryResponse, uuid.UUID, error) {
 	var response domain.PurchaseSummaryResponse
 	id, _ := uuid.NewV4()
 
 	if newPurchaseSummary.PaymentInfo.Installments > 6 {
 		interestRate, err := helpers.GetInterestRates("https://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados?formato=json")
 		if err != nil {
-			return response, err
+			return response, id, err
 		}
 
 		totalAmount := helpers.GetInstallmentsValue(newPurchaseSummary.ProductInfo.Price, newPurchaseSummary.PaymentInfo.DownPaymentAmount, newPurchaseSummary.PaymentInfo.Installments, interestRate)
@@ -41,7 +43,7 @@ func processPurchaseSummary(newPurchaseSummary domain.PurchaseSummary) (domain.P
 
 		err = insertPurchaseSummaryToRedis(id.String(), purchaseSummary)
 		if err != nil {
-			return response, err
+			return response, id, err
 		}
 
 		response = createPurchaseSummaryResponse(newPurchaseSummary, totalAmount, interestRate)
@@ -50,13 +52,12 @@ func processPurchaseSummary(newPurchaseSummary domain.PurchaseSummary) (domain.P
 
 		err := insertPurchaseSummaryToRedis(id.String(), purchaseSummary)
 		if err != nil {
-			return response, err
+			return response, id, nil
 		}
 
 		response = createPurchaseSummaryResponse(newPurchaseSummary, newPurchaseSummary.ProductInfo.Price, 0)
 	}
-
-	return response, nil
+	return response, id, nil
 }
 
 func createPurchaseSummary(newPurchaseSummary domain.PurchaseSummary, id uuid.UUID, totalAmount float64) domain.PurchaseSummary {
@@ -81,4 +82,26 @@ func createPurchaseSummaryResponse(newPurchaseSummary domain.PurchaseSummary, to
 		TotalAmount:  totalAmount,
 		InterestRate: interestRate,
 	}
+}
+
+func GetRedisPurchaseSummary(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	purchaseSummary, err := domain.FindByIDRedisPurchaseSumary(id)
+	if err != nil {
+		helpers.ErrorJSON(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	helpers.WriteJSON(w, http.StatusOK, purchaseSummary)
+
+}
+
+func CreateConfirmNewPurchase(w http.ResponseWriter, r *http.Request) {
+	var purchase domain.Purchase
+	if erro := json.NewDecoder(r.Body).Decode(&purchase); erro != nil {
+		helpers.ErrorJSON(w, erro, http.StatusBadRequest)
+		return
+	}
+
 }
